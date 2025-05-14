ce34a4c23830d16c08f5209e5608e42211ed3638
dhuoibisourour <dhuibisourour11@gmail.com>
2025-05-14 12:49:44 +0100

    Update BonLivraisonVente.js

--- back/models/BonLivraisonVente.js
+++ back/models/BonLivraisonVente.js
@@ -1,26 +1,58 @@
-// models/bonLivraisonVente.js
 const mongoose = require('mongoose');
 
-const BonLivraisonVenteSchema = new mongoose.Schema({
-  id_vente: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Vente', 
-    default: null 
+const BonLivraisonSchema = new mongoose.Schema({
+  reference: {
+    type: String,
+    unique: true
   },
-  id_client: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Client', 
-    default: null 
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Client',
+    required: true
   },
-  date_livraison: { 
-    type: Date, 
-    default: Date.now 
+  date: {
+    type: Date,
+    default: Date.now
   },
-  statut: { 
-    type: String, 
-    enum: ['En attente', 'Livré', 'Annulé'], 
-    default: 'En attente' 
+  adresseLivraison: String,
+  articles: [{
+    article: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Article',
+      required: true
+    },
+    quantiteLivree: {
+      type: Number,
+      required: true,
+      min: 1
+    },
+    prixHT: Number
+  }],
+  totalHT: Number,
+  statut: {
+    type: String,
+    default: 'Préparé',
+    enum: ['Préparé', 'Livré', 'Annulé']
   }
+}, { timestamps: true });
+
+// Génération automatique de la référence
+BonLivraisonSchema.pre('save', async function(next) {
+  if (!this.reference) {
+    const count = await mongoose.models.BonLivraison.countDocuments();
+    this.reference = `BL-${(count + 1).toString().padStart(4, '0')}`;
+  }
+  next();
 });
 
-module.exports = mongoose.model('BonLivraisonVente', BonLivraisonVenteSchema);
+// Calcul du total HT avant sauvegarde
+BonLivraisonSchema.pre('save', function(next) {
+  if (this.articles && this.articles.length > 0) {
+    this.totalHT = this.articles.reduce((total, item) => {
+      return total + (item.quantiteLivree * (item.prixHT || 0));
+    }, 0);
+  }
+  next();
+});
+
+module.exports = mongoose.model('BonLivraison', BonLivraisonSchema);
