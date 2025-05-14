250fc01981c33634037b1ca1083cd6d786f34159
dhuoibisourour <dhuibisourour11@gmail.com>
2025-05-14 13:23:43 +0100

    Update interventionsRoutes.js

--- back/routes/interventionsRoutes.js
+++ back/routes/interventionsRoutes.js
@@ -1,28 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { ajouterInterventionRepport,
-    ajouterIntervention, listeInterventions,
-     mettreAJourIntervention, supprimerIntervention,
-     mettreAJourInterventionRepport,
-     listeInterventionRepport,
-     getoneInterventionRepport
-     } = require('../controllers/interventions.contoller');
+const {
+  getInterventions,
+  getIntervention,
+  createIntervention,
+  updateIntervention,
+  deleteIntervention,
+  markAsCompleted
+} = require('../controllers/interventionController');
 
-// Route POST pour ajouter une intervention
-router.post('/ajouter', ajouterIntervention);
-router.post('/ajouterRepport', ajouterInterventionRepport);
-router.get('/all/report', listeInterventionRepport);
-router.get('/all/report/:id', getoneInterventionRepport);
+router.route('/')
+  .get(getInterventions)
+  .post(createIntervention);
 
-router.put('/all/report/:id', mettreAJourInterventionRepport);
+router.route('/:id')
+  .get(getIntervention)
+  .put(updateIntervention)
+  .delete(deleteIntervention);
 
-// Route GET pour lister toutes les interventions
-router.get('/', listeInterventions);
-
-// Route PUT pour mettre à jour une intervention
-router.put('/:id', mettreAJourIntervention);
-
-// Route DELETE pour supprimer une intervention
-router.delete('/:id', supprimerIntervention);
+router.put('/:id/complete', markAsCompleted);
 
 module.exports = router;
